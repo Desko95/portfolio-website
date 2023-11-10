767df8758df06503e2fc5948f2f943497792482e
Desko <simonedescode@gmail.com>
2023-11-10 19:45:22 +0000

    following tutorial at minute 9.43

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const HeroSection = () => {
   return (
     <section>
-        <h1 className='text-white'>Hello, I'm Simone</h1>
-    </section>,
-  )
-}
+      <div>
+        <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+          Hello, I'm Simone
+        </h1>
+        <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+          Metterti le penne nel culo non farà di te una gallina
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
