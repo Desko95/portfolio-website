a9475ea7057b1681d879425499055c0483efb266
Desko <simonedescode@gmail.com>
2023-11-10 19:06:46 +0000

    second step following tutorial min.7

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className='text-white'>Hello, I'm Simone</h1>
+    </section>;
+  );
+}
+
+export default HeroSection
