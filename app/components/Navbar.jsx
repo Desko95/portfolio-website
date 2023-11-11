d2e4c7256f55a919ca3b6e56d7b622c575dafe6a
Desko <simonedescode@gmail.com>
2023-11-11 19:06:45 +0000

    pre heroicons

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -19,13 +19,13 @@ const NavLinks = [
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-        <Link href={'/'} className='text-5xl text-white font-semibold'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+      <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
+        <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
           LOGO
         </Link>
         <div className='menu block md:w-auto' id='navbar'>
-          <ul>
+          <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
             {NavLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} />
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-b[#121212]">
       <Navbar />
-      <div class="container mx-auto px-12 py-4">
+      <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
      </div>
     </main>
