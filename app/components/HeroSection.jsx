6965c1d3f7ff9fec821b0ef5e2cc547d0be61459
Desko <simonedescode@gmail.com>
2023-11-11 17:18:25 +0000

    STARTING SECOND PART TUTORIAL MINUTE 24

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,7 +9,9 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500'>Hello, I'm{" "}</span> 
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500'>Hello, I'm{" "}
+            </span> 
+            <br></br>
             <TypeAnimation
               sequence={[
                 "Simone",
