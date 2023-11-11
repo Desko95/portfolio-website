50e152e6ace178a9d5381d3c8ccb934302989e7d
Desko <simonedescode@gmail.com>
2023-11-11 16:29:20 +0000

    gradient colors to header

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,14 +7,15 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-            Hello, I'm Simone
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500'>Hello, I'm{" "}</span> 
+            Simone
           </h1>
           <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
             Metterti le penne nel culo non farà di te una gallina
           </p>
           <div className='flex'> {/* Create a flex container */}
-            <button className='flex-1 px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-            <button className='flex-1 px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white'>Download CV</button>
+            <button className='flex-1 px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple500 to-green-500 hover:bg-slate-400 text-white'>Hire Me</button>
+            <button className='flex-1 px-6 py-3 w-full sm:w-fit  rounded-full bg-transparent hover:bg-slate-800 text-white border border-white'>Download CV</button>
           </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-4'>
