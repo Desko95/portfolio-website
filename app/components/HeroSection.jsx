7e96238bf6e6029261d759856bdf2d18bfcc9198
Desko <simonedescode@gmail.com>
2023-11-11 15:45:00 +0000

    avatar position alright!! savepoint

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -15,11 +15,11 @@ const HeroSection = () => {
           </p>
         </div>
         <div className='col-span-5'>
-          <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+          <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[340px] relative'>
             <Image
               src='/images/Avatar.png'
               alt='Avatar'
-              className='absolute transform -translate-x-1/2 translate-y-1/2 top-1/2 left-1/2'
+              className='absolute transform -translate-x-1/2 top-[-10] left-1/2' // avatar position
               width={300}
               height={300}
             />
