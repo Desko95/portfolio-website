79f5d20e930f316cd2533d13d8d5a4c53323bfb5
Desko <simonedescode@gmail.com>
2023-11-11 16:17:14 +0000

    tutorial minuto 18 fixato bottoni sovrapposti con chatgpt

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
 
-// https://www.youtube.com/watch?v=Kb1f5bvF6f4, this is the tutorial I'm following
 const HeroSection = () => {
   return (
     <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12'>
-        <div className='col-span-7 place-self-center'>
-          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
             Hello, I'm Simone
           </h1>
-          <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
             Metterti le penne nel culo non farà di te una gallina
           </p>
-          <div>
-            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-            <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+          <div className='flex'> {/* Create a flex container */}
+            <button className='flex-1 px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
+            <button className='flex-1 px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white'>Download CV</button>
           </div>
         </div>
-        <div className='col-span-5 place-self-center mt-4 lg:mt-4'>   
+        <div className='col-span-5 place-self-center mt-4 lg:mt-4'>
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[340px] relative'>
             <Image
               src='/images/Avatar.png'
               alt='Avatar'
-              className='absolute transform -translate-x-1/2 top-[-10] left-1/2' // avatar position + LINE 17= COORDINATES AVATAR 
+              className='absolute transform -translate-x-1/2 top-[-10] left-1/2'
               width={300}
               height={300}
             />
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import HeroSection from './components/HeroSection';
+import { Container } from 'postcss';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-b[#121212] container mx-auto">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-b[#121212]">
+      <div class="container mx-auto px-12 py-4">
+        <HeroSection />
+     </div>
     </main>
   )
 }
