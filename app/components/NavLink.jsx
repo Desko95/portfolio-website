388872a85101b2be910346cc500448ef85c9a9a2
Desko <simonedescode@gmail.com>
2023-11-11 18:57:36 +0000

    about projects contact section

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,1 +1,15 @@
-const NavLink = ({ }) 
+import Link from "next/link";
+
+const NavLink = ({ href, title }) => {
+    return (
+      <Link
+        href={href} // Use the 'href' prop passed to the component
+        className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
+      >
+        {title}
+      </Link>
+    );
+  };
+
+export default NavLink;
+  
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
+import NavLink from './NavLink';
+
+const NavLinks = [
+  {
+    title: "About",
+    path: "@about",
+  },
+  {
+    title: "Projects",
+    path: "@projects",
+  },
+  {
+    title: "Contact",
+    path: "@contact",
+  }
+]
 
 const Navbar = () => {
   return (
@@ -8,16 +24,14 @@ const Navbar = () => {
         <Link href={'/'} className='text-5xl text-white font-semibold'>
           LOGO
         </Link>
-        <div className='menu hidden md:block md:w-auto' id='navbar'>
+        <div className='menu block md:w-auto' id='navbar'>
           <ul>
-            <li>
-              <Link 
-                href={'#about'} 
-                className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
-              >
-                About
-              </Link>
-            </li>
+            {NavLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+            
           </ul>
         </div>
       </div>
