a92eee10ce2a31e68faeee3dabd45eefae3f3694
Desko <simonedescode@gmail.com>
2023-11-12 16:35:50 +0000

    Menu overlay done, starting About Me

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 import React, { useState }from 'react';
 import NavLink from './NavLink';
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import MenuOverlay from './MenuOverlay';
 
-const NavLinks = [
+const navLinks = [
   {
     title: "About",
     path: "@about",
@@ -23,7 +24,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'> {/*/ navbar opacity */}
       <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
         <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
           LOGO
@@ -43,7 +44,7 @@ const Navbar = () => {
           </div>
         <div className='menu hidden md:block md:w-auto' id='navbar'>
           <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
-            {NavLinks.map((link, index) => (
+            {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} />
               </li>
@@ -52,6 +53,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 }
