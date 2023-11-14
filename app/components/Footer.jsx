b3ca3822d881afa1c997f10889202a51084023b4
Desko <simonedescode@gmail.com>
2023-11-14 17:09:19 +0000

    funziona. bug fixati

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
         <div className="container p-12 flex justify-between">
-          <span className="text-muted">LOGO</span>
+          <span className="text-muted">Desko</span>
           <p className="text-slate-700">All right reserved.</p>
         </div>
       </footer>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'> {/*/ navbar opacity */}
       <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
         <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
-          LOGO
+          Desko
         </Link>
         <div className='mobile-menu block md:hidden:'>
           {
