c44ab5d6221ffd26198f9a0a4176126b0164dc4c
Desko <simonedescode@gmail.com>
2023-11-14 20:32:39 +0000

    ROUTE JS fix con zio

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,17 +1,17 @@
-//import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = await req.json();
-  const { email, subject, message } = body;
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail, // Email where I'll be sending the email
-      to: [''],
+      from: fromEmail,
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
@@ -22,7 +22,6 @@ export async function POST(req, res) {
         </>
       ),
     });
-
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
