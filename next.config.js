dc931c2252ed10c071f3d6ebe23dcee4b4e0e3d2
Desko <simonedescode@gmail.com>
2023-11-14 22:11:08 +0000

    next config  for export and deploy fixed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    images: { unoptimized: true },
+    distDir: "build"
+}
 
 module.exports = nextConfig
