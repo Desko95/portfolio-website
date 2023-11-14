700010618531f409fd879b99cec3d1d2fb773c5c
Desko <simonedescode@gmail.com>
2023-11-14 19:58:36 +0000

    fixed values and border achievement section

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const achievementsList = [
   {
@@ -22,12 +22,12 @@ const achievementsList = [
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row justify-between items-center">
+      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row justify-between items-center flex-wrap">
         {achievementsList.map((achievement, index) => {
           return (
             <div 
               key={index} 
-              className="flex flex-col items-center justify-center mx-4"
+              className="flex flex-col items-center justify-center mx-4 my-2 w-1/4 sm:w-auto"
             >
               <h2 className="text-white text-4xl font-bold">
                 {achievement.value}
@@ -41,5 +41,4 @@ const AchievementsSection = () => {
   );
 };
 
-
-export default AchievementsSection
+export default AchievementsSection;
