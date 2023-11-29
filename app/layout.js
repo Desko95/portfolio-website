ed20ca534fcca4b1eadadb074f0501b00bf0b27b
Desko <simonedescode@gmail.com>
2023-11-29 14:10:48 +0000

    page name fix

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Personal Portfolio',
+  title: 'Simone Personal Portfolio',
   description: 'by SimoneSD',
 }
 
