2cee861281070629edd7fb46eeece10db9473e14
Simone <simonedescode@gmail.com>
2023-11-30 16:23:22 +0000

    project section animation done

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
-//import { motion, useInView } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
   {
@@ -63,8 +63,8 @@ const projectsData = [
 
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
-  //const ref = useRef(null);
-  //const isInView = useInView(ref, { once: true });
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
   const handleTagChange = (newTag) => {
     setTag(newTag);
@@ -101,9 +101,15 @@ const ProjectsSection = () => {
           isSelected={tag === "Mobile"}
         />
       </div>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
-          
+          <motion.li
+            key={index}
+            variants={cardVariants} 
+            initial="initial" 
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4}}
+          >
             <ProjectCard
               key={project.id}
               title={project.title}
@@ -112,9 +118,9 @@ const ProjectsSection = () => {
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
             />
-          
+          </motion.li>
         ))}
-        </div>
+        </ul>
     </section>
   );
 };
