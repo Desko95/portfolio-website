5f9e9db48531e7d0cf872dd60f7689b132d0a076
Simone <simonedescode@gmail.com>
2023-12-06 17:50:32 +0000

    favicon replaced

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Simone Personal Portfolio',
+  title: "Simone's Personal Portfolio",
   description: 'by SimoneSD',
 }
 
